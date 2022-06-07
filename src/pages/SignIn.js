5f6e633e0d5344418ccfebc17a5f0541a28db64d
StudioPaap <anoukpaap@gmail.com>
2022-06-07 17:05:57 +0200

    huiswerk dinsdag

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
+import {AuthContext} from "../context/AuthContext";
+
 
 function SignIn() {
-  return (
-    <>
-      <h1>Inloggen</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
 
-      <form>
-        <p>*invoervelden*</p>
-        <button>Inloggen</button>
-      </form>
+    const {GaInloggen} = useContext(AuthContext);
 
-      <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
-    </>
-  );
+
+    return (
+        <>
+            <h1>Inloggen</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id
+                molestias qui quo unde?</p>
+
+            <form>
+                <p>*invoervelden*</p>
+                <button type="button"
+                        onClick={GaInloggen}
+                >Inloggen
+                </button>
+            </form>
+
+            <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
+        </>
+    );
+
 }
 
-export default SignIn;
+export default SignIn
